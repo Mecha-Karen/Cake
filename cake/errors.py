cbead0ccb956562904d80d980c4e001cdbbbe7b5
Seniatical <riotinghorse@Gmail.com>
2021-09-18 16:49:40 +0100

    Basic error types

---
+++ cake/errors.py
@@ -0,0 +1,5 @@
+class MissingValue(Exception):
+    r""" No value was specified for a var """
+
+class InvalidObject(Exception):
+    r""" Exception raised when the objest provided doesn't have a specific attribute/method """
