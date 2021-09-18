81f4512e61cfd6e9a7bf0ed6169615616cc084b7
Seniatical <riotinghorse@Gmail.com>
2021-09-18 21:00:05 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='Cake',
+  version='0.0.1a',
+  description='An object orientated math library',
+  long_description=open('README.md').read(),
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/Mecha-Karen/Cake", 
+  project_urls={
+   "Documentation": "https://docs.mechakaren.xyz/cake",
+   "Issue tracker": "https://github.com/Mecha-Karen/Cake/issues",
+   },
+  author='Seniatical',
+  license='MIT License', 
+  classifiers=classifiers,
+  keywords='Math,Python,OOP', 
+  packages=find_packages(),
+  install_requires=[] 
+)
