b14f7a060cd34ed67365cc47dd511537a1b9f327
Seniatical <riotinghorse@Gmail.com>
2021-09-20 17:29:41 +0100

    Simple working object for marking operators

--- cake/core/operator.py
+++ cake/core/operator.py
@@ -1,9 +1,28 @@
+from cake.abc import OPERATORS
+
+
 class Operator(object):
     __slots__ = ('op')
 
-    def __init__(self, *, op: str) -> None:
+    def __init__(self, op: str) -> None:
         self.op = op
 
+        if not self.op in OPERATORS:
+            raise TypeError('%s is not a valid operator' % self.op)
+
+    def __repr__(self) -> str:
+        return f"Operator({self.op})"
+
     def evaluate(self, left, right):
-        result = exec(f'x {self.op} y', {'x': left, 'y': right})
-        return result
+        r"""
+        Shortcut method of evaluating simple queries
+        """
+        if not self.op in OPERATORS:
+            raise TypeError('%s is not a valid operator' % self.op)
+
+        exec(f"""locals()['temp'] = {left} {self.op} {right}""")
+        return locals()['temp']
+
+    @property
+    def eval(self):
+        return self.evaluate
