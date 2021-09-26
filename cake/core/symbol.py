d5b2d8cf7b7003f074eec8f8afead33830ae1523
Seniatical <riotinghorse@Gmail.com>
2021-09-26 09:47:27 +0100

    Use set directly

--- cake/core/symbol.py
+++ cake/core/symbol.py
@@ -1,13 +1,13 @@
-ALLOWED = set((
+ALLOWED = {
     "(", ")", "{", "}", "[", "]",
-))
+}
 
 class Symbol(object):
     """
     A basic object which works like, Operator. This servers no purpose but to act as a marker for substitution and argument parsing.
     """
 
-    __slots__ = ('symbol')
+    __slots__ = ('value')
 
     def __init__(self, symbol: str) -> None:
         if not symbol in ALLOWED:
