ae2bdab7f7ab3cd019acfedcca91dcb47ba1fd34
Seniatical <riotinghorse@Gmail.com>
2021-10-08 15:51:02 +0100

    Start adding a few examples

---
+++ examples/algebra/starting.py
@@ -0,0 +1,28 @@
+# Getting started with algebra using cake
+
+# Import symbols or the Unknown object
+from cake import symbols
+from cake import Unknown
+
+# Unknown:
+x = Unknown("x")
+# Unknown(x)
+
+# If your lazy with methods you can use the `.parse` method
+x = Unknown.parse('x ** 2')
+# Unknown(x²)
+
+# Symbols function
+x = symbols(x)
+# Returns the same result as using the Object directly
+# Except you no longer have the privilege of the `parse` utility
+
+# Do some simple methods
+x += 10
+
+# Unknown(10 + x)
+
+# Substitute a new value in
+print(x.substitute(10))
+
+# Integer(20)
---
+++ examples/starting.py
@@ -0,0 +1,14 @@
+# Getting started with Cake!
+# Cake is an object-orientated math library, and aims to be like `sympy`
+
+# Documentation: https://docs.mechakaren.xyz/cake
+
+# Import the things you need:
+from cake import Integer
+
+# Simple addition with an integer
+x = Integer(10)
+
+print(x + 10)
+
+# Result: Integer(20)
