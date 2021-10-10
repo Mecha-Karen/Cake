7df69463636e17b67151b8d9893701dcb855e8e1
polo-sec <mattswann@protonmail.com>
2021-10-10 17:49:37 +0100

    Added support for basic Matrices operation: subtraction. Extending the Matrices function

--- cake/core/matrices.py
+++ cake/core/matrices.py
@@ -15,6 +15,21 @@ class Matrix:
         passedMatrixData.append([])
         lengthMatrixData = len(self.passedMatrixData[0])
         for k in range(lengthMatrixData):
+            # Addition operation
             passedMatrixData[j].append(self.passedMatrixData[j][k] + other.passedMatrixData[j][k])
+
+    return Matrix(passedMatrixData)
+
+  def __sub__(self, other):
+    # Modify the addition method to natively add matrices.
+    passedMatrixData = []
+
+    # Nested for loop to split and calculate each matrix item
+    for j in range(len(self.passedMatrixData)):
+        passedMatrixData.append([])
+        lengthMatrixData = len(self.passedMatrixData[0])
+        for k in range(lengthMatrixData):
+            # Subtraction operation
+            passedMatrixData[j].append(self.passedMatrixData[j][k] - other.passedMatrixData[j][k])
 
     return Matrix(passedMatrixData)
