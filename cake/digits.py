f0c3155527a7761e6873a1d7a31e5d582f987cb4
Seniatical <riotinghorse@Gmail.com>
2021-10-10 11:56:43 +0100

    Finish up docstrings

--- cake/digits.py
+++ cake/digits.py
@@ -4,12 +4,12 @@ import cake
 
 class Zero(cake.Integer):
     """
-    An object representing nothing, or simply the number ``0``
+    An object representing nothing, or simply the number ``0``.
+    This object inherits :class:`~cake.Integer`, you can find more infomation on how it works there!
 
     Parameters
     ----------
     check_value_attr: :class:`~typing.Optional[bool]`
-        
     """
 
     def __init__(self, check_value_attr: typing.Optional[bool] = False, *args, **kwargs):
