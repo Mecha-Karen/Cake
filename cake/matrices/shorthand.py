ec842962f45f0f9b6891a495064f625b3c92a076
Seniatical <riotinghorse@Gmail.com>
2021-10-15 13:39:11 +0100

    fix random matrix generation

--- cake/matrices/shorthand.py
+++ cake/matrices/shorthand.py
@@ -1,10 +1,11 @@
 # Shortcuts for creating different matrixes
 
+import typing
 import cake
 import random as rd
 
 
-def randomMatrix(col: int, row: int) -> "cake.Matrix":
+def randomMatrix(col: int = 3, row: int = 3, _range: typing.Tuple[int] = (0, 100)) -> "cake.Matrix":
     """
     Returns a matrix with random numbers
 
@@ -12,7 +13,12 @@ def randomMatrix(col: int, row: int) -> "cake.Matrix":
     ----------
     col: :class:`int`
         How many columns the matrix should have
+        Defaults to ``3``
     row: :class:`int`
         How many rows the matrix should have
+        Defaults to ``3``
+    _range: :class:`tuple`:
+        A tuple with 2 values, denoting the start and end for the random number selection.
+        Defaults to ``(0, 100)``
     """
-    return cake.Matrix(*[[rd.randint() for j in range(row)] for i in range(col)])
+    return cake.Matrix(*[[rd.randint(*_range) for j in range(row)] for i in range(col)])
