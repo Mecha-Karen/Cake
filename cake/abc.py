719490d87838f5a392f8dd67bfb4f4e7f564f2cb
Seniatical <isaja7710@gmail.com>
2021-10-29 20:30:03 +0100

    Seperate parsing abc from core abc

--- cake/abc.py
+++ cake/abc.py
@@ -1,7 +1,6 @@
-import cake
-import math
 import typing
 import string
+from math import pi, e
 
 # Types
 class IntegerType(typing.Protocol):
@@ -36,85 +35,4 @@ MAP_OPERATORS = {
     "modulus": "%",
 }
 
-KEYWORDS: typing.Mapping[str, typing.Callable] = {
-    "sqrt": math.sqrt,
-    "sin": lambda degrees: cake.Sin(degrees)(),
-    "cos": lambda degrees: cake.Cos(degrees)(),
-    "tan": lambda degrees: cake.Tan(degrees)(),
-    "cot": lambda degrees: cake.Integer(1) / (cake.Sin(degrees)()),
-    "sec": lambda degrees: cake.Integer(1) / (cake.Cos(degrees)()),
-    "cosec": lambda degrees: cake.Integer(1) / (cake.Tan(degrees)()),
-}
-
-SYMBOL_KW: typing.Mapping[str, typing.Callable] = {"!": math.factorial}
-
-CONSTANTS: typing.Mapping[str, IntegerType] = {"pi": math.pi, "e": math.e}
-
-SCALES: typing.Mapping[str, int] = {
-    "hundred": 1 * (10 ** 2),
-    "thousand": 1 * (10 ** 3),
-    "million": 1 * (10 ** 6),
-    "billion": 1 * (10 ** 9),
-    "trillion": 1 * (10 ** 12),
-}
-
-NUMBERS: typing.Mapping[str, int] = (
-    {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-        "ten": 10,
-        "eleven": 11,
-        "twelve": 12,
-        "thirteen": 13,
-        "fourteen": 14,
-        "fifteen": 15,
-        "sixteen": 16,
-        "seventeen": 17,
-        "eighteen": 18,
-        "nineteen": 19,
-        "twenty": 20,
-        "thirty": 30,
-        "forty": 40,
-        "fifty": 50,
-        "sixty": 60,
-        "seventy": 70,
-        "eighty": 80,
-        "ninety": 90,
-    },
-)
-
 ODD_NUMBERS: typing.Iterable[int] = filter(lambda _: _ % 2 != 0, range(100))
-
-# Prettifiers
-PRETTY_PRINT_SYMBOLS = {
-    "sqrt": "√",
-    "pi": "π",
-    "e": "e",
-    "powers": {
-        "0": "⁰",
-        "1": "¹",
-        "2": "²",
-        "3": "³",
-        "4": "⁴",
-        "5": "⁵",
-        "6": "⁶",
-        "7": "⁷",
-        "8": "⁸",
-        "9": "⁹",
-        "+": "⁺",
-        "-": "⁻",
-        "(": "⁽",
-        ")": "⁾",
-        "n": "ⁿ",
-    },
-}
-
-UNKNOWN_PRETTIFIER_SYMBOL = "?"
---
+++ cake/parsing/pABC.py
@@ -0,0 +1,61 @@
+import cake
+import typing
+from cake.abc import IntegerType, pi, e
+
+
+KEYWORDS: typing.Mapping[str, typing.Callable] = {
+    "sqrt": cake.Sqrt,
+    "sin": cake.Sin,
+    "cos": cake.Cos,
+    "tan": cake.Tan,
+    "cot": cake.Cot,
+    "sec": cake.Sec,
+    "cosec": cake.Cosec,
+}
+
+SYMBOL_KW: typing.Mapping[str, typing.Callable] = {
+    "!": cake.Factorial
+}
+
+CONSTANTS: typing.Mapping[str, IntegerType] = {"pi": pi, "e": e}
+
+SCALES: typing.Mapping[str, int] = {
+    "hundred": 1 * (10 ** 2),
+    "thousand": 1 * (10 ** 3),
+    "million": 1 * (10 ** 6),
+    "billion": 1 * (10 ** 9),
+    "trillion": 1 * (10 ** 12),
+}
+
+NUMBERS: typing.Mapping[str, int] = (
+    {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+        "eleven": 11,
+        "twelve": 12,
+        "thirteen": 13,
+        "fourteen": 14,
+        "fifteen": 15,
+        "sixteen": 16,
+        "seventeen": 17,
+        "eighteen": 18,
+        "nineteen": 19,
+        "twenty": 20,
+        "thirty": 30,
+        "forty": 40,
+        "fifty": 50,
+        "sixty": 60,
+        "seventy": 70,
+        "eighty": 80,
+        "ninety": 90,
+    },
+)
