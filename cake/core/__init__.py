43482f5a452b0d8a86df2cfce5f35de1327b5bbb
Seniatical <isaja7710@gmail.com>
2021-10-29 16:48:00 +0100

    Adjust unknown import

--- cake/core/__init__.py
+++ cake/core/__init__.py
@@ -1,6 +1,6 @@
 from .markers import *
-from .number import Number
 from .surd import *
-from .unknown.unknown import *
 
-from .types import *
+from .types import (Complex, Float, Integer, Irrational, Real, Prime)
+from .number import Number
+from .unknown.unknown import Unknown
