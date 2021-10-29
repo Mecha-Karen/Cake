9f736e10a139fc0248986f014147ce3f261ba992
Seniatical <isaja7710@gmail.com>
2021-10-29 20:08:24 +0100

    Add func to get closest function name

--- cake/functions/basic/__init__.py
+++ cake/functions/basic/__init__.py
@@ -1,2 +1,14 @@
 from .trig import *
 from .elem import *
+
+from .trig import __all__ as trigA
+from .elem import __all__ as elem
+
+from difflib import get_close_matches
+
+
+__all__ = trigA + elem
+
+
+def getClosestMatch(name: str) -> None:
+    return get_close_matches(name, __all__)
